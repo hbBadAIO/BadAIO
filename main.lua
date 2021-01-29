acb0fdb5f1edadf3e89ef9bcc34f60126553e8af
hbBadAIO <77203834+hbBadAIO@users.noreply.github.com>
2021-01-29 12:13:20 -0500

    Delete main.lua

--- main.lua
+++
@@ -1,6 +0,0 @@
-module.load(header.id, "champs/" .. player.charName:lower() .. "/main")
-module.load(header.id, "utility/deathping/main")
-
-chat.clear()
-chat.add("BadAIO Loaded!", {color = "#8b0000", bold = true})
-chat.print()
