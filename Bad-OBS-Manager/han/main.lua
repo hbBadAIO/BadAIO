22ee2b7f09dfb66bb5fc0ca5ac2c89e4f7bf14e8
hbBadAIO <77203834+hbBadAIO@users.noreply.github.com>
2021-01-29 12:26:27 -0500

    Update main.lua

--- Bad-OBS-Manager/han/main.lua
+++ Bad-OBS-Manager/han/main.lua
@@ -6,7 +6,7 @@ local function stream_switcher()
     args["data"] = {}
     if game.time == 30 then
         args["data"]["scene"] = "league_ingame"
-        network.send("127.0.0.1:4444/setscene", args)
+        network.send("127.0.0.1:5000/setscene", args)
     end
     if (obj.Manager.nexus[TEAM_ALLY].isDead == true) or (obj.Manager.nexus[TEAM_ENEMY].isDead == true) then
         args["data"]["scene"] = "league_outgame"
